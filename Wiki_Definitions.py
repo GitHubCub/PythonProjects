67267860b4fe99c8ca3a7c979b5046bc5a8618ec
Isabella Yuen <isabella.yuen@hotmail.com>
2016-04-14 21:51:44 +0100

    wkipedia definitions project

---
+++ Wiki_Definitions.py
@@ -0,0 +1,41 @@
+
+# coding: utf-8
+
+# # Wikipedia Definitions
+
+# In[7]:
+
+import urllib
+
+search_term = raw_input("What would you like to know about? (Please enter underscores instead of spaces) \n")
+search_url = "https://en.wikipedia.org/wiki/"+search_term
+
+get_wiki = urllib.urlopen(search_url)
+wiki_text = get_wiki.read()
+get_wiki.close()
+
+start_index = wiki_text.find("<p>")
+end_index = wiki_text.find("</p>")
+
+trimmed = wiki_text[start_index:end_index]
+
+def text_extraction(txt):
+    extract = ""
+    i=0
+    j=0
+    while i != -1:
+        i = txt.find("<",j)
+        j = txt.find(">",i)
+        if not txt[j+1] == "<":
+            extract = extract+txt[j+1:(txt[j+1:].find("<"))]
+    if txt[-1] == ".":
+        extract = extract+txt[(txt.rfind(">"))+1:]
+    return extract        
+        
+print text_extraction(trimmed) 
+
+
+# In[ ]:
+
+
+
